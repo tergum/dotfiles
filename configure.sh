de613e03958ca81e02d1f78a2cc933aa3d9e116e
Serhii Tarhonskyi <serhii@ubuntu-22.04>
2024-03-14 17:27:05 +0200

    Deploy dotfiles via simlinks

--- configure.sh
+++ configure.sh
@@ -9,16 +9,19 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --branches"
 
+CUR_PATH=$(pwd)
+echo $CUR_PATH
+
 mkdir ~/bin
 #script to create tmux session based on the current directory name
-cp bin/tat ~/bin
+ln -sfv ${CUR_PATH}/bin/tat ~/bin/tat
 # update user's .bashrc with tmux and vim integration commands
 cat .bashrc_update >> ~/.bashrc
 source ~/.bashrc
 
-cp .tmux.conf ~/.tmux.conf
+ln -sfv ${CUR_PATH}/.tmux.conf ~/.tmux.conf
 
 # Vim configuration
-cp .vimrc ~/.vimrc
+ln -sfv ${CUR_PATH}/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
